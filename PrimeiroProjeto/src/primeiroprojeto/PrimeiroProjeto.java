961aa65f0629c8560f127c2490da053e1701b3ee
higor <higor@higor-H81>
2024-02-15 12:05:53 -0300

    Começando meu estudos em java, Criando meu primeiro programa.

---
+++ PrimeiroProjeto/src/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primeiroprojeto;
+
+/**
+ *
+ * @author higor
+ */
+public class PrimeiroProjeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Criando meu primeiro programa em java");
+        System.out.println("Olá, Mundo");
+        System.out.println("Já me livrei da maldição...");
+    }
+    
+}
