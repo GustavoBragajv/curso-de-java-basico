ce4f49257f81ec469eb2c86abc3be0b87e0876d8
higor <higor@localhost>
2024-03-08 19:31:09 -0300

    Estudando conceitos sobre Tratamentos de Erros (StackOverflow)

---
+++ TratamentoErrosStackOverflow/src/tratamentoerrosstackoverflow/TratamentoErrosStackOverflow.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tratamentoerrosstackoverflow;
+
+/**
+ *
+ * @author higor
+ */
+public class TratamentoErrosStackOverflow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Recursividade();
+        
+    }
+    
+    
+    public static void Recursividade() {
+        Recursividade();
+    }
+    
+}
